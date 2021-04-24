33e9693b6e2d8bf62ecf4039a2c0d06fbb3fb605
shudal <shudal@qq.com>
2021-04-25 04:07:55 +0800

    udpate

--- Source/block/UI/EventCardUserWidget.cpp
+++ Source/block/UI/EventCardUserWidget.cpp
@@ -5,6 +5,7 @@
 #include "EventCardItem.h"
 #include "../Config/StrConst.h"
 #include "Components/TextBlock.h"
+#include "MainUserWidget.h"
 
 void UEventCardUserWidget::SetListItemObject(UEventCardItem* i) {
 	auto j = i->GetEventCardItem();
@@ -14,5 +15,11 @@ void UEventCardUserWidget::SetListItemObject(UEventCardItem* i) {
 	FJsonSerializer::Serialize(j.ToSharedRef(), JsonWriter); 
 
 	//TB_EType->SetText(FText::FromString(j->GetStringField(StrConst::Get().EVENT_TYPE)));
-	TB_EType->SetText(FText::FromString(OutputString));
+	//TB_EType->SetText(FText::FromString(OutputString));
+
+	auto eventJO = j->GetObjectField("event");
+
+	auto event_type_idx = eventJO->GetIntegerField(StrConst::Get().EVENT_TYPE);
+	FString event_type_strs[5]{ "ShouldNotExist","Object","Aggregation","Transformation","Transaction"};
+	TB_EType->SetText(FText::FromString(event_type_strs[event_type_idx]));
 }
