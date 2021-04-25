7e1b65cad492112fd9d7cb90fef9d3d079f47c14
shudal <shudal@qq.com>
2021-04-25 10:09:23 +0800

    udpate

--- Source/block/UI/EventCardUserWidget.cpp
+++ Source/block/UI/EventCardUserWidget.cpp
@@ -5,6 +5,7 @@
 #include "EventCardItem.h"
 #include "../Config/StrConst.h"
 #include "Components/TextBlock.h"
+#include "Components/UniformGridPanel.h"	
 #include "MainUserWidget.h"
 
 void UEventCardUserWidget::SetListItemObject(UEventCardItem* i) {
@@ -22,4 +23,121 @@ void UEventCardUserWidget::SetListItemObject(UEventCardItem* i) {
 	auto event_type_idx = eventJO->GetIntegerField(StrConst::Get().EVENT_TYPE);
 	FString event_type_strs[5]{ "ShouldNotExist","Object","Aggregation","Transformation","Transaction"};
 	TB_EType->SetText(FText::FromString(event_type_strs[event_type_idx]));
+
+	TB_uuid->SetText(FText::FromString(eventJO->GetStringField(StrConst::Get().UUID)));
+	TB_action->SetText(FText::FromString(eventJO->GetStringField(StrConst::Get().ACTION)));
+	TB_etime->SetText(FText::FromString(eventJO->GetStringField(StrConst::Get().EVENT_TIME)));
+	TB_recordtime->SetText(FText::FromString(eventJO->GetStringField(StrConst::Get().RECORD_TIME)));
+	TB_tzoff->SetText(FText::FromString(eventJO->GetStringField(StrConst::Get().EVENT_TIME_ZONE_OFFSET)));
+
+	FString default_value = "None";
+	{
+		FString s = default_value;
+		bool x = eventJO->TryGetStringField(StrConst::Get().BIZ_STEP, s);
+		if (s.IsEmpty()) s = default_value;
+		TB_bizstep->SetText(FText::FromString(s));
+	} 
+	{
+		FString s = default_value;
+		bool x = eventJO->TryGetStringField(StrConst::Get().READ_POINT, s);
+		if (s.IsEmpty()) s = default_value;
+		TB_readpoint->SetText(FText::FromString(s));
+	}
+	{
+		FString s = default_value;
+		bool x = eventJO->TryGetStringField(StrConst::Get().BIZ_LOCATION, s);
+		if (s.IsEmpty()) s = default_value;
+		TB_bizloc->SetText(FText::FromString(s));
+	}
+	{
+		FString s = default_value;
+		bool x = eventJO->TryGetStringField(StrConst::Get().DISPOSITION, s);
+		if (s.IsEmpty()) s = default_value;
+		TB_disposition->SetText(FText::FromString(s));
+	}
+	{
+		FString s = default_value;
+		bool x = eventJO->TryGetStringField(StrConst::Get().PARENT_ID, s);
+		if (s.IsEmpty()) s = default_value;
+		TB_parentid->SetText(FText::FromString(s));
+	}
+	{
+		FString s = default_value;
+		bool x = eventJO->TryGetStringField(StrConst::Get().TRANSFORMATION_ID, s);
+		if (s.IsEmpty()) s = default_value;
+		TB_transid->SetText(FText::FromString(s));
+	}
+
+	{
+		TArray<TSharedPtr<FJsonValue>> arr; 
+		const TArray<TSharedPtr<FJsonValue>>* arrp = &arr;
+		bool x = eventJO->TryGetArrayField(StrConst::Get().BIZ_TRANSACTION_LIST, arrp);
+		if (x) {
+			auto s = FormatMyTypeValue(arrp);
+			TB_biztrans->SetText(FText::FromString(s));
+		}
+	}
+	{
+		TArray<TSharedPtr<FJsonValue>> arr;
+		const TArray<TSharedPtr<FJsonValue>>* arrp = &arr;
+		bool x = eventJO->TryGetArrayField(StrConst::Get().SOURCE_LIST, arrp);
+		if (x) {
+			auto s = FormatMyTypeValue(arrp);
+			TB_sourlist->SetText(FText::FromString(s));
+		}
+	}
+	{
+		TArray<TSharedPtr<FJsonValue>> arr;
+		const TArray<TSharedPtr<FJsonValue>>* arrp = &arr;
+		bool x = eventJO->TryGetArrayField(StrConst::Get().DESTINATION_LIST, arrp);
+		if (x) {
+			auto s = FormatMyTypeValue(arrp);
+			TB_destlist->SetText(FText::FromString(s));
+		}
+	}
+	EEventType et = static_cast<EEventType>(event_type_idx);
+	switch (et) {
+	case EEventType::Object:
+		UGP_parentid->SetVisibility(ESlateVisibility::Collapsed); 
+		UGP_transid->SetVisibility(ESlateVisibility::Collapsed);
+		
+
+		UGP_inputepcs->SetVisibility(ESlateVisibility::Collapsed);
+		UGP_outepcs->SetVisibility(ESlateVisibility::Collapsed);
+		UGP_inquanlist->SetVisibility(ESlateVisibility::Collapsed);
+		UGP_outquanlist->SetVisibility(ESlateVisibility::Collapsed);
+		break;
+	case EEventType::Aggregation:
+		UGP_transid->SetVisibility(ESlateVisibility::Collapsed);
+
+		UGP_inputepcs->SetVisibility(ESlateVisibility::Collapsed);
+		UGP_outepcs->SetVisibility(ESlateVisibility::Collapsed);
+		UGP_inquanlist->SetVisibility(ESlateVisibility::Collapsed);
+		UGP_outquanlist->SetVisibility(ESlateVisibility::Collapsed);
+		break;
+	case EEventType::Transformation:
+		UGP_parentid->SetVisibility(ESlateVisibility::Collapsed);
+		break;
+	case EEventType::Transaction:
+		UGP_parentid->SetVisibility(ESlateVisibility::Collapsed);
+
+		UGP_inputepcs->SetVisibility(ESlateVisibility::Collapsed);
+		UGP_outepcs->SetVisibility(ESlateVisibility::Collapsed);
+		UGP_inquanlist->SetVisibility(ESlateVisibility::Collapsed);
+		UGP_outquanlist->SetVisibility(ESlateVisibility::Collapsed);
+		break;
+	}
+}
+
+FString UEventCardUserWidget::FormatMyTypeValue(const TArray<TSharedPtr<FJsonValue>>* arrp) {
+	FString s = "";
+	for (auto& p : *arrp) {
+		TSharedPtr<FJsonObject> j = p->AsObject();
+		FString subs = "";
+		auto t = j->GetStringField(StrConst::Get().TYPEVALUE_TYPE);
+		auto v = j->GetStringField(StrConst::Get().TYPEVALUE_VALUE);
+		subs = FString::Printf(TEXT("Type: %s; Value: %s"), *t, *v);
+		s = s + subs + "\n";
+	}
+	return s;
 }
--- Source/block/UI/EventCardUserWidget.h
+++ Source/block/UI/EventCardUserWidget.h
@@ -14,10 +14,76 @@ class BLOCK_API UEventCardUserWidget : public UUserWidget
 {
 	GENERATED_BODY()
 
+private:
+	FString FormatMyTypeValue(const TArray<TSharedPtr<FJsonValue>>* arrp);
 protected:
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Meta = (BindWidget))
 		class UTextBlock* TB_EType = nullptr;
-	
+
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Meta = (BindWidget))
+		class UTextBlock* TB_uuid = nullptr;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Meta = (BindWidget))
+		class UTextBlock* TB_etime = nullptr;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Meta = (BindWidget))
+		class UTextBlock* TB_recordtime = nullptr;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Meta = (BindWidget))
+		class UTextBlock* TB_tzoff = nullptr;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Meta = (BindWidget))
+		class UTextBlock* TB_bizstep = nullptr;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Meta = (BindWidget))
+		class UTextBlock* TB_readpoint = nullptr;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Meta = (BindWidget))
+		class UTextBlock* TB_bizloc = nullptr;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Meta = (BindWidget))
+		class UTextBlock* TB_disposition = nullptr;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Meta = (BindWidget))
+		class UTextBlock* TB_parentid = nullptr;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Meta = (BindWidget))
+		class UTextBlock* TB_transid = nullptr; 
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Meta = (BindWidget))
+		class UTextBlock* TB_action = nullptr;
+
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Meta = (BindWidget))
+		class UUniformGridPanel* UGP_parentid = nullptr;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Meta = (BindWidget))
+		class UUniformGridPanel* UGP_transid = nullptr;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Meta = (BindWidget))
+		class UUniformGridPanel* UGP_epclist = nullptr;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Meta = (BindWidget))
+		class UUniformGridPanel* UGP_quantitylist = nullptr;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Meta = (BindWidget))
+		class UUniformGridPanel* UGP_inputepcs = nullptr;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Meta = (BindWidget))
+		class UUniformGridPanel* UGP_outepcs = nullptr;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Meta = (BindWidget))
+		class UUniformGridPanel* UGP_inquanlist= nullptr;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Meta = (BindWidget))
+		class UUniformGridPanel* UGP_outquanlist = nullptr;
+
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Meta = (BindWidget))
+		class UTextBlock* TB_biztrans = nullptr;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Meta = (BindWidget))
+		class UTextBlock* TB_sourlist = nullptr;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Meta = (BindWidget))
+		class UTextBlock* TB_destlist = nullptr;
+
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Meta = (BindWidget))
+		class UTextBlock* TB_epclist = nullptr;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Meta = (BindWidget))
+		class UTextBlock* TB_quantitylist = nullptr;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Meta = (BindWidget))
+		class UTextBlock* TB_inputepcs = nullptr;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Meta = (BindWidget))
+		class UTextBlock* TB_outepcs = nullptr;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Meta = (BindWidget))
+		class UTextBlock* TB_inquanlist = nullptr;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Meta = (BindWidget))
+		class UTextBlock* TB_outquanlist = nullptr;
 public:
 
 	UFUNCTION(BlueprintCallable)
